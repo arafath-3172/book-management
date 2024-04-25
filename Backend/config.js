6545dce3675e65dfa673fe5cc640c5c534cda755
Arafath Qureshi <arfathq9@gmail.com>
2024-04-26 01:16:22 +0530

    final

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://arafathq9:arafath@book.75usyns.mongodb.net/?retryWrites=true&w=majority&appName=Book";
+// export const mongoDBURL =
+//   "mongodb+srv://arafathq9:arafath@book.75usyns.mongodb.net/?retryWrites=true&w=majority&appName=Book";
+export const mongoDBURL = "mongodb://mongodb:27017/db";
